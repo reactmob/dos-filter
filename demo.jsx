7f55e49ce64d8dc56fb956904d3ee62309d4e4d7
liverbool <nukboon@gmail.com>
2017-08-08 03:23:59 +0700

    fix demo

--- demo.jsx
+++ demo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Set } from 'immutable';
 
 import './style.scss';
 import { Filter, RemoteConfig, FilterList, StringType, NumberType, DateType, SelectType, ExistsType } from 'reactmob-filter';
