3973a79a2baec1e8a80d3f3598ac0d7c260ea983
liverbool <nukboon@gmail.com>
2017-08-08 03:16:08 +0700

    fix demo

--- demo.jsx
+++ demo.jsx
@@ -25,8 +25,8 @@ const countries = [
 
 const countryOptions = {
     items: countries,
-    valueField: 'id',
-    displayField: item => `${item['firstName']} ${item['lastName']}`,
+    valueField: 'abbr',
+    displayField: item => `${item['abbr']} - ${item['name']}`,
     /*items: RemoteConfig.create({
         url: 'http://127.0.0.1:8899/api/v1/customers/',
         parser: (data) => {
